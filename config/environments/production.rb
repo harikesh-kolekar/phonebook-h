3bcd48df1af6e8de8a12d87ade7f61a11136bf35
ReshmaKolekar <reshmawalekar264@gmail.com>
2020-12-07 15:39:59 +0530

    cpmmented

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Email
+  # Email
   config.action_mailer.default_url_options = { :host => 'phonebook-kolekar.rhcloud.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
